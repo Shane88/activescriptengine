6af01b923a3fc297e0fa5e5a40571caeaa9cbf22
Shane88 <4056111+Shane88@users.noreply.github.com>
2014-03-08 20:26:16 +1300

    Added unit tests for NewInstanceOf extension method.

---
+++ ActiveXScriptLibUnitTests/JScriptNewInstanceOf_Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace ActiveXScriptLibUnitTests
+{
+   using ActiveXScriptLib.Extensions;
+   using FluentAssertions;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   public class JScriptNewInstanceOf_Tests : JScriptTestBase
+   {
+      [TestInitialize]
+      public void TestSetup()
+      {
+         scriptEngine.AddCode(@"
+            // Define a class like this
+            function Person(){
+               // Add object properties like this
+               this.Name = "";
+            }");
+      }
+
+      [TestMethod]
+      public void When_I_create_an_instance_of_a_class()
+      {
+         // Arrange.
+         // Act.
+         dynamic person = scriptEngine.NewInstanceOf("Person");
+
+         person.Name = "Name set from C#";
+
+         // Assert.
+         string name = person.Name;
+
+         name.Should().Be("Name set from C#");
+      }
+   }
+}
---
+++ ActiveXScriptLibUnitTests/VBScriptNewInstanceOf_Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace ActiveXScriptLibUnitTests
+{
+   using ActiveXScriptLib.Extensions;
+   using FluentAssertions;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   [TestClass]
+   public class VBScriptNewInstanceOf_Tests : VBScriptTestBase
+   {
+      [TestInitialize]
+      public void TestSetup()
+      {
+         scriptEngine.AddCode(
+            @"Class Person
+                 Public Name
+                 Public DateOfBirth
+              End Class");
+      }
+
+      [TestMethod]
+      public void When_I_create_an_instance_of_a_class()
+      {
+         // Arrange.
+         // Act.
+         dynamic person = scriptEngine.NewInstanceOf("Person");
+
+         person.Name = "Name set from C#";
+
+         // Assert.
+         string name = person.Name;
+         name.Should().Be("Name set from C#");
+      }
+   }
+}
